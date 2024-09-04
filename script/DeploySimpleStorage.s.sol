9fa22ecfc1a05a5a98d8df3a69cb629d5fbf4ba4
eldanniklk <danisabcheezzz@gmail.com>
2024-09-04 16:07:44 +0200

    actuelizacion del submodulo forge-std

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import { Script} from "forge-std/Script.sol";
+import { Script} from "forge-std/Script.sol"; 
 import { SimpleStorage} from "../src/SimpleStorage.sol";
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
